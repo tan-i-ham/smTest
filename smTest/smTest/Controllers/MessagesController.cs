fcf4502712c97cc367987d3f0da402eb2e56a923
tp6hannah <hannah51125@gmail.com>
2017-08-29 22:56:21 +0800

    simplify function

--- smTest/smTest/Controllers/MessagesController.cs
+++ smTest/smTest/Controllers/MessagesController.cs
@@ -32,37 +32,15 @@ namespace smTest
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 Activity reply = activity.CreateReply();
                 //await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
-
+                
+                //user傳一張照片
                 if (activity.Attachments?.Count > 0 && activity.Attachments.First().ContentType.StartsWith("image"))//IF NULL 不會往下,有東西才繼續run
                 {
-                    //user傳一張照片
-                    string uriName = decodeQRCode(reply, activity.Attachments.First().ContentUrl);
-                    Uri uriResult;
-                    bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
-                        && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-                    if (result == true)
-                    {
+                   string uriName = decodeQRCode(reply, activity.Attachments.First().ContentUrl);
+                   string rt = await PassScrapTextAsync(uriName);
+                   reply.Text = rt+ uriName;
 
-                        var infos = await DetailsFromPage(uriName);
-                        string alltext = "作業日期\t\t作業種類\t\t作業內容\n\n============================\n\n";
-
-                        foreach (var info in infos)
-                        {
-                            alltext += String.Join(", ", info.Date);
-                            alltext += "\t\t";
-                            alltext += String.Join(", ", info.Type);
-                            alltext += "\t\t";
-                            alltext += String.Join(", ", info.Content);
-                            alltext += "\n\n";
-
-                            //table.Rows.Add(info.Date, info.Type, info.Content, info.Ref);
-                        }
-                        reply.Text = alltext;
-                    }
-                    else
-                    {
-                        reply.Text = result.ToString() + " 不是網址!";
-                    }
+                   
                 }
                 else if (activity.ChannelId == "facebook")
                 {
@@ -92,7 +70,7 @@ namespace smTest
                                 new CardAction(){Title="去產銷履歷網站", Type=ActionTypes.OpenUrl, Value="http://taft.coa.gov.tw/"},
                             }
                         };
-                        await connector.Conversations.ReplyToActivityAsync(reply);
+                        
                     }
 
                     else if (activity.Text == "上傳QR code")
@@ -106,8 +84,6 @@ namespace smTest
                             decodeQRCode(reply, activity.Attachments.First().ContentUrl);
    
                         }
-
-                        await connector.Conversations.ReplyToActivityAsync(reply);
                     }
 
                     else if (fbData.message.quick_reply != null)
@@ -115,42 +91,16 @@ namespace smTest
                         reply.Text = $"your choice is {fbData.message.quick_reply.payload}";
                     }
                  
-                    else if (activity.Text == "scrap")
-                    {
-                        
-                    }
-                  
 
                     else
                     {
-                        string uriName = activity.Text;
-                        Uri uriResult;
-                        bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
-                            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-                        if (result == true)
-                        {
+                        //如果傳網址 一樣爬的到
 
-                            var infos = await DetailsFromPage(uriName);
-                            string alltext = "作業日期\t\t作業種類\t\t作業內容\n\n============================\n\n";
-
-                            foreach (var info in infos)
-                            {
-                                alltext += String.Join(", ", info.Date);
-                                alltext += "\t\t";
-                                alltext += String.Join(", ", info.Type);
-                                alltext += "\t\t";
-                                alltext += String.Join(", ", info.Content);
-                                alltext += "\n\n";
-
-                                //table.Rows.Add(info.Date, info.Type, info.Content, info.Ref);
-                            }
-                            reply.Text = alltext;
-                        }
-                        else
-                        {
-                            reply.Text = result.ToString() + " 不是網址!";
-                        }
-
+                        string rt = await PassScrapTextAsync(activity.Text);
+                        reply.Text = "!!!!" + rt + "!!!!";
+                        
+                        //reply.Text = $"echo:{activity.Text}";
+                        
                     }
                 }
 
@@ -161,21 +111,40 @@ namespace smTest
             return response;
         }
 
+        private async Task<String> PassScrapTextAsync(string uriName)
+        {
+
+            Uri uriResult;
+            bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (result == true)
+            {
+
+                var infos = await DetailsFromPage(uriName);
+                string alltext = "作業日期\t\t作業種類\t\t作業內容\n\n============================\n\n";
+
+                foreach (var info in infos)
+                {
+                    alltext += String.Join(", ", info.Date);
+                    alltext += "\t\t";
+                    alltext += String.Join(", ", info.Type);
+                    alltext += "\t\t";
+                    alltext += String.Join(", ", info.Content);
+                    alltext += "\n\n";
+
+                    //table.Rows.Add(info.Date, info.Type, info.Content, info.Ref);
+                }
+                return alltext;
+            }
+            else
+            {
+                return result.ToString() + " 不是網址!";
+            }
+        }
+
+        //讀取QrCode
         private string decodeQRCode(Activity reply, string url)
         {
-            /* List<Attachment> att = new List<Attachment>();
-            att.Add(new HeroCard() //建立fb ui格式的api
-            {
-                Title = "Cognitive services",
-                Subtitle = "Select from below",
-                Images = new List<CardImage>() { new CardImage(url) },
-                Buttons = new List<CardAction>()
-                {
-                    new CardAction(ActionTypes.PostBack, "辨識圖片", value: $"Analyze>{url}"),//帶json payload
-                    new CardAction(ActionTypes.PostBack, "decode QR", value: $"Analyze>{url}")
-                }
-            }.ToAttachment());*/
-
             // create a barcode reader instance
             IBarcodeReader reader = new BarcodeReader();
             // load a bitmap
@@ -184,14 +153,12 @@ namespace smTest
             var result = reader.Decode(barcodeBitmap);
             // do something with the result
             if (result != null)
-            {
-                //txtDecoderType.Text = result.BarcodeFormat.ToString();
-                //reply.Text = result.Text;
+            { 
                 return result.Text;
             }
             return result.Text;
-            //reply.Attachments = att;
         }
+
         //讀取圖片
         private Bitmap ImageFromWeb(string url)
         {
